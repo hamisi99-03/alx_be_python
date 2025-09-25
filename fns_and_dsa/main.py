aab37912fee80269f8188d5c12e0b0d089d461b7
Hamisi <yuzalsif2@gmail.com>
2025-09-25 20:08:50 +0300

    scripted shooping list file

--- fns_and_dsa/main.py
+++ fns_and_dsa/main.py
@@ -1,13 +1,36 @@
-from arithmetic_operations import perform_operation
+from shopping_list_manager import add_item, remove_item, view_list
+def display_menu():
+    print("Shopping List Manager")
+    print("1. Add Item")
+    print("2. Remove Item")
+    print("3. View List")
+    print("4. Exit")
 
 def main():
-    print("Arithmetic Operations")
-    num1 = float(input("Enter the first number: "))
-    num2 = float(input("Enter the second number: "))
-    operation = input("Enter the operation (add, subtract, multiply, divide): ").strip().lower()
+    shopping_list = []
+    while True:
+        display_menu()
+        choice = input("Enter your choice: ")
 
-    result = perform_operation(num1, num2, operation)
-    print(f"Result: {result}")
+        if choice == '1':
+            # Prompt for and add an item
+            add_item(shopping_list, input("Enter item to add: "))
+            pass
+        elif choice == '2':
+            # Prompt for and remove an item
+            remove_item = (shopping_list,input("Enter item to remove: "))
+            pass
+        elif choice == '3':
+            # Display the shopping list
+            print("Current Shopping List:", view_list(shopping_list))
+            pass
+        elif choice == '4':
+            print("Goodbye!")
+            break
+        else:
+            print("Invalid choice. Please try again.")
+
+
 
 if __name__ == "__main__":
     main()
