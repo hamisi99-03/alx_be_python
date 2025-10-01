5e134d8d6a495071307f316078d1b4225f519ba0
Hamisi <yuzalsif2@gmail.com>
2025-10-01 18:44:03 +0300

    added testing script

---
+++ programming_paradigm/test_simple_calculator.py
@@ -0,0 +1,28 @@
+import unittest
+from simple_calculator  import SimpleCalculator
+
+class TestSimpleCalculator(unittest.TestCase):
+    def setUp(self):
+        self.calc = SimpleCalculator()
+    def test_addition(self):
+        self.assertEqual(self.calc.add(1,3),4)
+        self.assertEqual(self.calc.add(-1,1),0)
+        self.assertEqual(self.calc.add(-1,-1),-2)
+    def test_subtract(self):
+        self.assertEqual(self.calc.subtract(4,3),1)
+        self.assertEqual(self.calc.subtract(5,-1),6)
+        self.assertEqual(self.calc.subtract(-1,-1),0)
+
+    def test_divide(self):
+        self.assertEqual(self.calc.divide(4,2),2)
+        self.assertEqual(self.calc.divide(5,-1),-5)
+        self.assertEqual(self.calc.divide(-1,-1),1)
+        with self.assertRaises(ValueError):
+          self.calc.divide(10,0)
+    def test_multiply(self):
+        self.assertEqual(self.calc.multiply(4,3),12)
+        self.assertEqual(self.calc.multiply(5,-1),-5)
+        self.assertEqual(self.calc.multiply(-1,-1),1)
+if __name__ == '__main__':
+    unittest.main()
+
